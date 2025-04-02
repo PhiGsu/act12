7e54091d5fef95d99ca2df7275d0673af22c1b8f
Phi Le <phizzle2003@gmail.com>
2025-04-02 13:56:58 -0400

    Finish implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
+import 'package:act12/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(InventoryApp());
 }
 
 class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,11 +32,101 @@ class InventoryHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _InventoryHomePageState createState() => _InventoryHomePageState();
+  State<InventoryHomePage> createState() => _InventoryHomePageState();
 }
 
 class _InventoryHomePageState extends State<InventoryHomePage> {
-// TODO: Implement Firestore integration
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final CollectionReference _products =
+      FirebaseFirestore.instance.collection('products');
+  late Stream<QuerySnapshot<Object?>> products;
+
+  @override
+  void initState() {
+    super.initState();
+    products = _products.snapshots();
+  }
+
+  Future<void> _createOrUpdate([DocumentSnapshot? documentSnapshot]) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _nameController.text = documentSnapshot['name'];
+      _priceController.text = documentSnapshot['price'].toString();
+    }
+
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext ctx) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(ctx).viewInsets.bottom + 20,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                controller: _priceController,
+                decoration: const InputDecoration(
+                  labelText: 'Price',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                child: Text(action == 'create' ? 'Create' : 'Update'),
+                onPressed: () async {
+                  String name = _nameController.text;
+                  double? price = double.tryParse(_priceController.text);
+                  if (name.isNotEmpty && price != null) {
+                    if (action == 'create') {
+                      await _products.add({"name": name, "price": price});
+                    }
+
+                    if (action == 'update') {
+                      await _products.doc(documentSnapshot!.id).update({
+                        "name": name,
+                        "price": price,
+                      });
+                    }
+
+                    _nameController.text = '';
+                    _priceController.text = '';
+
+                    if (mounted) {
+                      Navigator.of(context).pop();
+                    }
+                  }
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _deleteProduct(String productId) async {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('You have successfully deleted a product'),
+      ),
+    );
+
+    await _products.doc(productId).delete();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,33 +138,55 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Inventory Management System',
+            StreamBuilder(
+              stream: products,
+              builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                if (streamSnapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: streamSnapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        final DocumentSnapshot documentSnapshot =
+                            streamSnapshot.data!.docs[index];
+                        return Card(
+                          margin: const EdgeInsets.all(10),
+                          child: ListTile(
+                            title: Text(documentSnapshot['name']),
+                            subtitle: Text(documentSnapshot['price'].toString()),
+                            trailing: SizedBox(
+                              width: 100,
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.edit),
+                                    onPressed: () =>
+                                        _createOrUpdate(documentSnapshot),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.delete, color: Colors.red),
+                                    onPressed: () =>
+                                        _deleteProduct(documentSnapshot.id),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
+                return const Text('No products found');
+              },
             ),
-
-// TODO: Implement inventory list view
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-// TODO: Implement add inventory item
-        },
+        onPressed: () => _createOrUpdate(),
         tooltip: 'Add Item',
         child: Icon(Icons.add),
       ),
     );
   }
-}
-
-// id string
-// name string
-// amount double
-
-class Product {
-  final String id;
-  final String name;
-  final num price;
-
-  Product({required this.id, required this.name, required this.price});
 }
